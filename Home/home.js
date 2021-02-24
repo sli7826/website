c7a72bf0a2ec035552cb5629c2fd708520866ce3
sli7826 <sli94@u.rochester.edu>
2021-02-24 21:36:12 +0000

    commit things

---
+++ Home/home.js
@@ -0,0 +1,35 @@
+// reads in our .env file and makes those values available as environment variables
+require('dotenv').config();
+ 
+const express = require('express');
+const bodyParser = require('body-parser');
+ 
+// create an instance of an express app
+const app = express();
+ 
+// update express settings
+app.use(bodyParser.urlencoded({ extended: false })); // parse application/x-www-form-urlencoded
+app.use(bodyParser.json()); // parse application/json
+ 
+// main routes
+app.get('/status', (req, res, next) => {
+  res.status(200);
+  res.json({ 'status': 'ok' });
+});
+ 
+// catch all other routes
+app.use((req, res, next) => {
+  res.status(404);
+  res.json({ message: '404 - Not Found' });
+});
+ 
+// handle errors
+app.use((err, req, res, next) => {
+  res.status(err.status || 500);
+  res.json({ error : err });
+});
+ 
+// have the server start listening on the provided port
+app.listen(process.env.PORT || 3000, () => {
+  console.log(`Server started on port ${process.env.PORT || 3000}`);
+});
