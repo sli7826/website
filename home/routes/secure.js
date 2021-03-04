efe94e331b20f37059a64c6e7a59b557b258004c
sli7826 <sli94@u.rochester.edu>
2021-03-04 06:58:29 +0000

    commit things

--- home/routes/secure.js
+++ home/routes/secure.js
@@ -15,4 +15,10 @@ router.get('/scores', asyncMiddleware(async (req, res, next) => {
   res.status(200).json(users);
 }));
 
+router.post('/upload', asyncMiddleware(async (req, res, next) => {
+  const { email, score } = req.body;
+  await UserModel.updateOne({ email }, { highScore: score });
+  res.status(200).json({ status: 'ok' });
+}));
+
 module.exports = router;
